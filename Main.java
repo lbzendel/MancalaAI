9042932d005e4dfc3c2e1165d52a31e004170c66
Lior Zendel <liorbenzendel@gmail.com>
2026-02-22 14:49:31 -0500

    Initial commit

--- Main.java
+++ Main.java
@@ -19,7 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        while (sc.hasNextInt()) {
+        while (sc.hasNextLine()) {
+            if (!sc.hasNextInt()) {
+                sc.nextLine();
+                continue;
+            }
             GameState state = parseGameState(sc);
             System.out.println("1");
         }
